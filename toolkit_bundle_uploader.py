fcfde623179a94e97d1c2359e39a64080c58c83d
kikeda <kikeda@falconstreehouse.com>
2023-07-20 19:10:18 +0900

    Add log basicconfig

--- toolkit_bundle_uploader.py
+++ toolkit_bundle_uploader.py
@@ -3,6 +3,13 @@ import logging
 
 import shotgun_api3
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.FileHandler("log.txt")]  # Add a file handler
+)
+
 # Constants for ShotGrid API connection
 SG_BASE_URL = os.environ.get("SG_BASE_URL")
 SG_SCRIPT_NAME = os.environ.get("SG_SCRIPT_NAME")
